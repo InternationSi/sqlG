862abe9eca1cd38fe6fc6225278f73bb43b5a067
xiaosi <sam2020888@outlook.com>
2023-04-28 18:05:25 +0800

    fix

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: sfy
+ * @Date: 2023-04-26 22:52:25
+ * @LastEditors: sfy
+ * @LastEditTime: 2023-04-28 18:03:19
+ * @FilePath: /sqlG/.dumirc.ts
+ * @Description: update here
+ */
 import { defineConfig } from 'dumi';
 
 export default defineConfig({
@@ -5,4 +13,5 @@ export default defineConfig({
   themeConfig: {
     name: 'sqlG',
   },
+  base: '/sqlG'
 });
--- docs-dist/dumi__pages__Demo.57d1da89.async.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunksqlG=self.webpackChunksqlG||[]).push([[448],{90553:function(l,n,e){e.r(n),e.d(n,{default:function(){return s}});var a=e(91005),o=e(50959),r=function(){var u=(0,a.UO)(),d=u.id,m=(0,a.WF)(),c=m.demos,f=c[d]||{},t=f.component;return t&&(0,o.createElement)(t)},s=r}}]);
